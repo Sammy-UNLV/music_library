260062ad57391e1c6e071fae285242385702eb55
Samuel Fisher <skiy23@outlook.com>
2022-04-17 16:45:46 -0700

    Update artist view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
+import React, { Fragment } from 'react'
 
-const App = () => {
+function App() {
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
@@ -12,31 +16,42 @@ const App = () => {
     useEffect(() => {
         if(search) {
             const fetchData = async () => {
-                document.title = `${search} music`
+                document.title = `${search} Music`
                 const response = await fetch(API_URL + search)
                 const resData = await response.json()
                 if (resData.results.length > 0) {
                     return setData(resData.results)
                 } else {
-                    return setMessage('Not Found.')
+                    return setMessage('Not Found')
                 }
             }
             fetchData()
         }
     }, [search])
-
+    
     const handleSearch = (e, term) => {
         e.preventDefault()
         setSearch(term)
     }
-
     return (
         <div>
-            <SearchBar handleSearch={handleSearch} />
-            {message}
-            <Gallery data={data} />
+        {message}
+            <Router>
+                <Routes>
+                    <Route path="/" element={
+                        <Fragment>
+                            <SearchBar handleSearch = {handleSearch}/>
+                            <Gallery data={data} />
+                        </Fragment>
+                    } />
+                    <Route path="/album/:id" element={<AlbumView />} />
+                    <Route path="/artist/:id" element={<ArtistView />} />
+                </Routes>
+            </Router>
         </div>
-    )
+    );
+    
 }
 
-export default App
+export default App;
+
